3205bdce249a63618e7726768d54af9fda9a38a5
pawan rana <pawan.pvt.ltd@gmail.com>
2020-05-31 19:56:02 +0530

    Updates

---
+++ precache-manifest.0ba5b0fcabf3f1f2f66311c3d2dd0fb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b91dd4f871161c319de",
+    "url": "/puzzle-test/static/css/main.793e5d52.chunk.css"
+  },
+  {
+    "revision": "6b91dd4f871161c319de",
+    "url": "/puzzle-test/static/js/main.db997a74.chunk.js"
+  },
+  {
+    "revision": "087252bde404418071ca",
+    "url": "/puzzle-test/static/js/runtime~main.1c3a1625.js"
+  },
+  {
+    "revision": "18cdae6cac2d39820a4e",
+    "url": "/puzzle-test/static/js/2.92a8fa96.chunk.js"
+  },
+  {
+    "revision": "d744e6839752b2e5d2e3b584c943de9e",
+    "url": "/puzzle-test/static/media/shuffle.d744e683.svg"
+  },
+  {
+    "revision": "dc3bf2a0e732b55fbc70978bab6b6c54",
+    "url": "/puzzle-test/index.html"
+  }
+];
